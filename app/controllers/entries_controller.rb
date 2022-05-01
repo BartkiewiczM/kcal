ab20bd7b899c39259f1c0f7106c1ac3d3cf01e6e
BartkiewiczM <228684@edu.p.lodz.pl>
2022-05-01 16:10:15 +0200

    deploy to heroku

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,7 +5,7 @@ class EntriesController < ApplicationController
   # GET /entries or /entries.json
   def index
     @entries = Entry.where("created_at >= ? AND user_id == ?", Date.today, current_user)
-
+  
   end
 
   # GET /entries/1 or /entries/1.json
